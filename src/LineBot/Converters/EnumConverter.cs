a381fc66ea14068178d95092347fb68e53a11d7d
Dirk Lemstra <dirk@git.imagemagick.org>
2017-04-08 10:12:00 +0000

    Added json converter for enum strings.

---
+++ src/LineBot/Converters/EnumConverter.cs
@@ -0,0 +1,49 @@
+﻿// Copyright 2017 Dirk Lemstra (https://github.com/dlemstra/line-bot-sdk-dotnet)
+//
+// Dirk Lemstra licenses this file to you under the Apache License,
+// version 2.0 (the "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at:
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+using System;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace Line
+{
+    internal sealed class EnumConverter<TEnum> : JsonConverter
+        where TEnum : struct
+    {
+        public override bool CanWrite => false;
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.GetTypeInfo().IsEnum;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType != JsonToken.String)
+                throw new InvalidOperationException("Only string is supported.");
+
+            if (!Enum.TryParse((string)reader.Value, true, out TEnum result))
+            {
+                result = default(TEnum);
+            }
+
+            return result;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
