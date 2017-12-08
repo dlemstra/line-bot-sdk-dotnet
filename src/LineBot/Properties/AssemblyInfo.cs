34b61fb640435496cb5579198f36eb957a13bc0d
Dirk Lemstra <dirk@git.imagemagick.org>
2017-12-08 10:25:26 +0100

    Published Line.Bot.SDK 1.1.0.

--- src/LineBot/Properties/AssemblyInfo.cs
+++ src/LineBot/Properties/AssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright 2017 Dirk Lemstra")]
 [assembly: AssemblyTitle("LineBot")]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
