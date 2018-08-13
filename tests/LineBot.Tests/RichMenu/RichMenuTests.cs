9b3e966c4259072ca3506685dc3f3d374a83a799
Ben67366 <ben67366@gmail.com>
2018-08-13 18:30:41 +0900

    Add more unit tests.

--- tests/LineBot.Tests/RichMenu/RichMenuTests.cs
+++ tests/LineBot.Tests/RichMenu/RichMenuTests.cs
@@ -34,16 +34,16 @@ namespace Line.Tests
         [TestMethod]
         public async Task CreateRichMenu_CorrectInput_CallsApi()
         {
-            var richMenu = new RichMenu()
+            var richMenu = new RichMenu
             {
                 ChatBarText = "testChatBarTxt",
                 Name = "testName",
-                RichMenuAreas = new RichMenuArea[]
+                RichMenuAreas = new[]
                 {
                     new RichMenuArea
                     {
                         Action = new UriAction { Label = "testLabel", Url = new Uri("http://www.google.com") },
-                        RichMenuBounds = new RichMenuBounds()
+                        RichMenuBounds = new RichMenuBounds
                         {
                             Height = 100,
                             Width = 100,
@@ -54,16 +54,16 @@ namespace Line.Tests
                     new RichMenuArea
                     {
                         Action = new UriAction { Label = "testLabel2", Url = new Uri("http://www.bing.com") },
-                        RichMenuBounds = new RichMenuBounds()
+                        RichMenuBounds = new RichMenuBounds
                         {
                             Height = 200,
                             Width = 200,
                             X = 100,
                             Y = 0
                         }
-                    },
+                    }
                 },
-                RichMenuSize = new RichMenuSize() { Height = 1686, Width = 2500 },
+                RichMenuSize = new RichMenuSize { Height = 1686, Width = 2500 },
                 Selected = false
             };
 
@@ -79,6 +79,205 @@ namespace Line.Tests
             Assert.AreEqual(postedData, httpClient.PostedData);
 
             Assert.AreEqual(result, "richmenu-801b2cd26b2f13587329ed501d279d27");
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionWhenNameIsMoreThan300Chars()
+        {
+            var richMenu = new RichMenu();
+
+            ExceptionAssert.Throws<InvalidOperationException>("The name cannot be longer than 300 characters.", () =>
+            {
+                richMenu.Name = new string('x', 301);
+            });
+        }
+
+        [TestMethod]
+        public void ShouldNotThrowExceptionWhenNameIs300Chars()
+        {
+            var value = new string('x', 300);
+
+            var richMenu = new RichMenu
+            {
+                Name = value
+            };
+
+            Assert.AreEqual(value, richMenu.Name);
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionWhenChatBarTextIsMoreThan14Chars()
+        {
+            var richMenu = new RichMenu();
+
+            ExceptionAssert.Throws<InvalidOperationException>("The chat bar text cannot be longer than 14 characters.", () =>
+            {
+                richMenu.ChatBarText = new string('x', 15);
+            });
+        }
+
+        [TestMethod]
+        public void ShouldNotThrowExceptionWhenChatBarTextIs14Chars()
+        {
+            var value = new string('x', 14);
+
+            var richMenu = new RichMenu
+            {
+                ChatBarText = value
+            };
+
+            Assert.AreEqual(value, richMenu.ChatBarText);
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionWhenRichMenuAreasMoreThan20()
+        {
+            var richMenuArea = new RichMenuArea
+            {
+                Action = new UriAction { Label = "testLabel2", Url = new Uri("http://www.bing.com") },
+                RichMenuBounds = new RichMenuBounds { Height = 200, Width = 200, X = 100, Y = 0 }
+            };
+
+            var value = new RichMenuArea[]
+            {
+                richMenuArea, richMenuArea, richMenuArea, richMenuArea, richMenuArea,
+                richMenuArea, richMenuArea, richMenuArea, richMenuArea, richMenuArea,
+                richMenuArea, richMenuArea, richMenuArea, richMenuArea, richMenuArea,
+                richMenuArea, richMenuArea, richMenuArea, richMenuArea, richMenuArea,
+                richMenuArea
+            };
+
+            var richMenu = new RichMenu();
+
+            ExceptionAssert.Throws<InvalidOperationException>("The maximum number of areas is 20.", () =>
+            {
+                richMenu.RichMenuAreas = value;
+            });
+        }
+
+        [TestMethod]
+        public void ShouldNotThrowExceptionWhenRichMenuAreasIs20()
+        {
+            var richMenuArea = new RichMenuArea
+            {
+                Action = new UriAction { Label = "testLabel2", Url = new Uri("http://www.bing.com") },
+                RichMenuBounds = new RichMenuBounds { Height = 200, Width = 200, X = 100, Y = 0 }
+            };
+            var value = new RichMenuArea[]
+            {
+                richMenuArea, richMenuArea, richMenuArea, richMenuArea, richMenuArea,
+                richMenuArea, richMenuArea, richMenuArea, richMenuArea, richMenuArea,
+                richMenuArea, richMenuArea, richMenuArea, richMenuArea, richMenuArea,
+                richMenuArea, richMenuArea, richMenuArea, richMenuArea, richMenuArea
+            };
+
+            var richMenu = new RichMenu();
+            richMenu.RichMenuAreas = value;
+
+            Assert.AreEqual(value, richMenu.RichMenuAreas);
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionWhenRichMenuBoundsIsNull()
+        {
+            var richMenuArea = new RichMenuArea();
+
+            ExceptionAssert.Throws<InvalidOperationException>("The bounds cannot be null.", () =>
+            {
+                richMenuArea.RichMenuBounds = null;
+            });
+        }
+
+        [TestMethod]
+        public void ShouldNotThrowExceptionWhenRichMenuBoundsIsNotNull()
+        {
+            var richMenuBounds = new RichMenuBounds { Height = 200, Width = 200, X = 100, Y = 0 };
+
+            var richMenuArea = new RichMenuArea
+            {
+                RichMenuBounds = richMenuBounds
+            };
+
+            Assert.AreEqual(richMenuBounds, richMenuArea.RichMenuBounds);
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionWhenActionIsNull()
+        {
+            var richMenuArea = new RichMenuArea();
+
+            ExceptionAssert.Throws<InvalidOperationException>("The action cannot be null.", () =>
+            {
+                richMenuArea.Action = null;
+            });
+        }
+
+        [TestMethod]
+        public void ShouldNotThrowExceptionWhenActionIsNotNull()
+        {
+            var action = new UriAction { Label = "testLabel2", Url = new Uri("http://www.bing.com") };
+
+            var richMenuArea = new RichMenuArea
+            {
+                Action = action
+            };
+
+            Assert.AreEqual(action, richMenuArea.Action);
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionWhenWidthIsNot2500()
+        {
+            var richMenuSize = new RichMenuSize();
+
+            ExceptionAssert.Throws<InvalidOperationException>("The width must be 2500!", () =>
+            {
+                richMenuSize.Width = 100;
+            });
+        }
+
+        [TestMethod]
+        public void ShouldNotThrowExceptionWhenWidthIs2500()
+        {
+            var richMenuSize = new RichMenuSize()
+            {
+                Width = 2500
+            };
+
+            Assert.AreEqual(2500, richMenuSize.Width);
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionWhenHeightIsNot1686_843()
+        {
+            var richMenuSize = new RichMenuSize();
+
+            ExceptionAssert.Throws<InvalidOperationException>("The Possible height values: 1686, 843.", () =>
+            {
+                richMenuSize.Height = 100;
+            });
+        }
+
+        [TestMethod]
+        public void ShouldNotThrowExceptionWhenHeightIs1686()
+        {
+            var richMenuSize = new RichMenuSize()
+            {
+                Height = 1686
+            };
+
+            Assert.AreEqual(1686, richMenuSize.Height);
+        }
+
+        [TestMethod]
+        public void ShouldNotThrowExceptionWhenHeightIs843()
+        {
+            var richMenuSize = new RichMenuSize()
+            {
+                Height = 843
+            };
+
+            Assert.AreEqual(843, richMenuSize.Height);
         }
     }
 }
