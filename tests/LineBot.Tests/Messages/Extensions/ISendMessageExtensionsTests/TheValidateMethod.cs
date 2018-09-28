fe6963a030d546bae4549d441623d276edaf3bdb
Dirk Lemstra <dirk@lemstra.org>
2018-09-28 02:03:33 +0200

    Added extra unit tests.

--- tests/LineBot.Tests/Messages/Extensions/ISendMessageExtensionsTests/TheValidateMethod.cs
+++ tests/LineBot.Tests/Messages/Extensions/ISendMessageExtensionsTests/TheValidateMethod.cs
@@ -57,6 +57,104 @@ namespace Line.Tests
                     ISendMessageExtensions.Validate(new ISendMessage[1] { new TextMessage() });
                 });
             }
+
+            [TestMethod]
+            public void ShouldNotThrowExceptionWhenMessageIsTextMessage()
+            {
+                var message = new TextMessage() { Text = "Foo" };
+
+                ISendMessageExtensions.Validate(new ISendMessage[1] { message });
+            }
+
+            [TestMethod]
+            public void ShouldNotThrowExceptionWhenMessageIsImageMessage()
+            {
+                var message = new ImageMessage()
+                {
+                    Url = new Uri("https://foo.bar"),
+                    PreviewUrl = new Uri("https://bar.foo")
+                };
+
+                ISendMessageExtensions.Validate(new ISendMessage[1] { message });
+            }
+
+            [TestMethod]
+            public void ShouldNotThrowExceptionWhenMessageIsVideoMessage()
+            {
+                var message = new VideoMessage()
+                {
+                    Url = new Uri("https://foo.bar"),
+                    PreviewUrl = new Uri("https://bar.foo")
+                };
+
+                ISendMessageExtensions.Validate(new ISendMessage[1] { message });
+            }
+
+            [TestMethod]
+            public void ShouldNotThrowExceptionWhenMessageIsAudioMessage()
+            {
+                var message = new AudioMessage()
+                {
+                    Url = new Uri("https://foo.bar"),
+                    Duration = 1
+                };
+
+                ISendMessageExtensions.Validate(new ISendMessage[1] { message });
+            }
+
+            [TestMethod]
+            public void ShouldNotThrowExceptionWhenMessageIsLocationMessage()
+            {
+                var message = new LocationMessage()
+                {
+                    Title = "Foo",
+                    Address = "Bar"
+                };
+
+                ISendMessageExtensions.Validate(new ISendMessage[1] { message });
+            }
+
+            [TestMethod]
+            public void ShouldNotThrowExceptionWhenMessageIsStickerMessage()
+            {
+                var message = new StickerMessage()
+                {
+                    PackageId = "1",
+                    StickerId = "2"
+                };
+
+                ISendMessageExtensions.Validate(new ISendMessage[1] { message });
+            }
+
+            [TestMethod]
+            public void ShouldNotThrowExceptionWhenMessageIsImagemapMessage()
+            {
+                var message = new ImagemapMessage()
+                {
+                    BaseUrl = new Uri("https://foo.bar"),
+                    AlternativeText = "Foo",
+                    BaseSize = new ImagemapSize(10, 10),
+                    Actions = new[] { new ImagemapUriAction("https://foo.bar", new ImagemapArea(1, 2, 3, 4)) }
+                };
+
+                ISendMessageExtensions.Validate(new ISendMessage[1] { message });
+            }
+
+            [TestMethod]
+            public void ShouldNotThrowExceptionWhenMessageIsTemplageMessage()
+            {
+                var message = new TemplateMessage()
+                {
+                    AlternativeText = "Foo",
+                    Template = new ButtonsTemplate()
+                    {
+                        Text = "Foo",
+                        Actions = new[] { new MessageAction() { Label = "Foo", Text = "Bar" } }
+                    }
+                };
+
+                ISendMessageExtensions.Validate(new ISendMessage[1] { message });
+            }
         }
     }
 }
