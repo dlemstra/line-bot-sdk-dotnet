56db71f575e440154480138f36f2b752417f8dc0
Dirk Lemstra <dirk@lemstra.org>
2018-11-09 08:06:58 +0100

    Added Label property to ImagemapMessageAction and ImagemapUriAction.

---
+++ tests/LineBot.Tests/Messages/Imagemap/Actions/ImagemapUriActionTests/TheLabelProperty.cs
@@ -0,0 +1,50 @@
+﻿// Copyright 2017-2018 Dirk Lemstra (https://github.com/dlemstra/line-bot-sdk-dotnet)
+//
+// Dirk Lemstra licenses this file to you under the Apache License,
+// version 2.0 (the "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at:
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Line.Tests
+{
+    public partial class ImagemapUriActionTests
+    {
+        [TestClass]
+        public class TheLabelProperty
+        {
+            [TestMethod]
+            public void ShouldThrowExceptionWhenValueIsMoreThan50Chars()
+            {
+                var action = new ImagemapMessageAction();
+
+                ExceptionAssert.Throws<InvalidOperationException>("The label cannot be longer than 50 characters.", () =>
+                {
+                    action.Label = new string('x', 51);
+                });
+            }
+
+            [TestMethod]
+            public void ShouldNotThrowExceptionWhenValueIs50Chars()
+            {
+                string value = new string('x', 50);
+
+                var action = new ImagemapMessageAction()
+                {
+                    Label = value
+                };
+
+                Assert.AreEqual(value, action.Label);
+            }
+        }
+    }
+}
