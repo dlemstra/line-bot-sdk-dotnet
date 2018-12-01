08c42b608cc3e467e3399c407e87f3e0c0da499a
Dirk Lemstra <dirk@lemstra.org>
2018-12-01 10:23:13 +0100

    Sign the assembly with a strong name.

--- src/LineBot/Properties/InternalsVisibleTo.cs
+++ src/LineBot/Properties/InternalsVisibleTo.cs
@@ -15,4 +15,9 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("LineBot.Tests")]
+[assembly: InternalsVisibleTo("LineBot.Tests, PublicKey = " +
+"00240000048000009400000006020000002400005253413100040000010001006909ef8e9f192e" +
+"aa8026ee87798f69319d87e3ca4bf47bdb6d967fce05ec000db0ba575e47079c5b8a519bf5b609" +
+"a99574d9831b5dbf091e02ce85fbb99032821ff6683cc5328e4e23ee3b736b0a3026bcdf2f10e3" +
+"470bae2c5e6a5b5eb2a51619d61a4afed72bf1edd27372b2975c18e05ac7a54e24d1d60cb310b6" +
+"b46e7ad2")]
