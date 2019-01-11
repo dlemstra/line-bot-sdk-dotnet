57affbe45fb70ea7a93e1bc8f2f89712586da906
Dirk Lemstra <dirk@lemstra.org>
2019-01-11 21:46:46 +0100

    Added extra check.

--- tests/LineBot.Tests/LineBotTests/TheGetEventsMethod.cs
+++ tests/LineBot.Tests/LineBotTests/TheGetEventsMethod.cs
@@ -13,6 +13,7 @@
 // under the License.
 
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -62,6 +63,7 @@ namespace Line.Tests
                 var events = await bot.GetEvents(request);
 
                 Assert.IsNotNull(events.Events);
+                Assert.AreEqual(0, events.Events.Count());
                 Assert.AreEqual("xxxxxxxxxx", events.Destination);
             }
         }
